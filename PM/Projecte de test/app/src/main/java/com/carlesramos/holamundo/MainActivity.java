176ac0b09506e5d5f50f255b65748ee0e0ea7375
carlesRa <jcramos8282@gmail.com>
2019-09-17 18:50:06 +0200

    eercici07 fet

---
+++ PM/Projecte de test/app/src/main/java/com/carlesramos/holamundo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.carlesramos.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
