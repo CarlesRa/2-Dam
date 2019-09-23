93d2cb7d566eea1d7d566a0a145f07d7f2263171
carlesRa <jcramos8282@gmail.com>
2019-09-23 11:20:55 +0200

    exercici3 fet

---
+++ AAD/exercicis/exercicisProgramació/activitat1 unitat1/Exercici03.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+public class Exercici03{
+    public static void main(String[] args) throws IOException {
+        File fichero1;
+        File fichero2;
+        BufferedReader lector;
+        FileWriter escritor;
+        String linea;
+        try{
+            fichero1 = new File("./f3.txt");
+            fichero2 = new File("./f4.txt");
+            lector = new BufferedReader(new FileReader(fichero1));
+            escritor = new FileWriter(fichero2,true);
+            
+            while((linea = lector.readLine()) != null){
+                escritor.write("\n" + linea);
+            }
+            lector.close();
+            escritor.close();
+        }catch(FileNotFoundException fnfe){
+            System.out.println("No se encuentra el fichero");
+        }
+    }
+}
