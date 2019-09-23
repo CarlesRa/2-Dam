71eca3cf62d94e38f34bcc056cd059d538dfea1a
carlesRa <jcramos8282@gmail.com>
2019-09-23 11:34:52 +0200

    exercici04 fet

---
+++ AAD/exercicis/exercicisProgramació/activitat1 unitat1/Exercici04.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Exercici04{
+    public static void main(String[] args) throws IOException{
+        File fichero;
+        BufferedReader lector;
+        int contadorLineas;
+        fichero = new File("./f4.txt");
+        lector = new BufferedReader(new FileReader(fichero));//abrimos el flujo de entrada
+        contadorLineas = 0;
+        while (lector.readLine() != null){
+            contadorLineas++;
+        }
+        lector.close();//cerramos el flujo
+        System.out.println("En el fichero " + fichero.getName() + " que esta en: " + fichero.getAbsolutePath()
+        + " hay: " + contadorLineas + " lineas.");
+    }
+}
