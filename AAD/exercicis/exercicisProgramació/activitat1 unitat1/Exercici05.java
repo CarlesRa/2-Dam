a5b0e9181a56c629df89d8eef7969f4b588f197c
carlesRa <jcramos8282@gmail.com>
2019-09-23 12:02:10 +0200

    Exercici 05 comlet

---
+++ AAD/exercicis/exercicisProgramació/activitat1 unitat1/Exercici05.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+import java.io.IOException;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Exercici05{
+    public static void main(String[] args) throws IOException{
+        BufferedReader lector;
+        File fichero;
+        String linea;
+        Scanner lec;
+        int numeroDeLineas;
+        fichero = new File(args[0]);
+        lector = new BufferedReader(new FileReader(fichero));
+        lec = new Scanner(System.in);
+        System.out.print("Cueantas lineas desea ver?: ");
+        numeroDeLineas = lec.nextInt();
+        lec.nextLine();
+        for(int i=0; i<numeroDeLineas; i++){
+            if((linea = lector.readLine()) != null){
+                System.out.println("Linea numero " + (i+1) + ": " +linea);
+            }
+            else{
+                System.out.println("\u001B[31mNo hay mas lineas que mostrar!!\u001B[0m");
+                return;
+            }
+        }
+        lector.close();
+    }
+}
