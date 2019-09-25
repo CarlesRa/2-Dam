2f1011fb501a00d128d5f0d255464d2313656d03
carlesRa <jcramos8282@gmail.com>
2019-09-25 08:49:30 +0200

    fique nous arxius

--- PMU/primeraapp/app/src/main/java/com/carlesramos/primeraapp/MainActivity.java
+++ PMU/primeraapp/app/src/main/java/com/carlesramos/primeraapp/MainActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+
+
+
         Toast.makeText(this,"Mac es una gena",Toast.LENGTH_LONG).show();
         Log.d(MainActivity.this.getLocalClassName(),"onCreate()");
     }
